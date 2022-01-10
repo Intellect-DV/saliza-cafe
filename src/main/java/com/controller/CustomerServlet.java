a1c499701d148cfd26d4fdf0049cc8f79a696784
Aiman Anizan <anizanaimann1@gmail.com>
2022-01-11 01:37:31 +0800

    Add customer servlet and signup,login method

---
+++ src/main/java/com/controller/CustomerServlet.java
@@ -0,0 +1,59 @@
+package com.controller;
+
+import com.dataacess.CustomerDA;
+import com.model.Customer;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "CustomerServlet", value = "/customer?action=login")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+
+        switch(action.toLowerCase()){
+            case("signup"):
+                signUp(request,response);
+                break;
+            case("login"):
+                login(request, response);
+                break;
+
+            default:
+                // other
+                break;
+        }
+    }
+
+    private void signUp(HttpServletRequest request, HttpServletResponse response){
+        // perform sign up
+        String username = request.getParameter("username");
+
+        Customer cust = CustomerDA.isUsernameExisted(username);
+
+        if(cust.isValid()) {
+            // set data
+            String password, name, email;
+            password = request.getParameter("password");
+            name = request.getParameter("name");
+            email = request.getParameter("email");
+
+            cust.setCustomer(username, password, name, email);
+
+            // add new customer
+            CustomerDA.addCustomer(cust);
+        }
+    }
+
+    private void login(HttpServletRequest request, HttpServletResponse response) {
+        // perform login
+    }
+}
