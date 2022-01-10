7042887f2d6949f0fe31aaaa8a48015a8d67647b
Aiman Anizan <anizanaimann1@gmail.com>
2022-01-11 03:09:24 +0800

    Change url value

--- src/main/java/com/controller/CustomerServlet.java
+++ src/main/java/com/controller/CustomerServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "CustomerServlet", value = "/customer?action=login")
+@WebServlet(name = "CustomerServlet", value = "/customer")
 public class CustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
