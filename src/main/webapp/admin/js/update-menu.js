24778ae0791085853da869143dd0a25111d237de
Aiman Anizan <anizanaimann1@gmail.com>
2022-01-24 23:04:32 +0800

    Add http request to update menu

--- src/main/webapp/admin/js/update-menu.js
+++ src/main/webapp/admin/js/update-menu.js
@@ -6,6 +6,11 @@ const inputDescription = document.querySelector("input[name='description']");
 
 window.addEventListener("DOMContentLoaded", () => {
     getMenuInfo();
+
+    formUpdate.addEventListener("submit", event => {
+        event.preventDefault();
+        updateMenuInfo();
+    })
 })
 
 const getMenuInfo = () => {
@@ -17,6 +22,25 @@ const getMenuInfo = () => {
             inputName.value = content.menuName;
             inputPrice.value = content.menuPrice;
             inputDescription.value = content.menuDescription;
+        })
+        .catch(err => {
+            console.log(err.response.data);
+        })
+}
+
+const updateMenuInfo = () => {
+    const formData = new FormData(formUpdate);
+    const params = new URLSearchParams();
+    const url = '/menu?action=updatemenuinfo';
+
+    params.append("id", menuId);
+    for(let key of formData.keys()) {
+        params.append(key, String(formData.get(key)));
+    }
+
+    axios.post(url, params)
+        .then(response => {
+            console.log(response.data);
         })
         .catch(err => {
             console.log(err.response.data);
