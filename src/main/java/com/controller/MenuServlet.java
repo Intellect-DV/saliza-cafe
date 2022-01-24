d8ede8e3c169bcd3c04d88af34f6b7680516ea30
Aiman Anizan <anizanaimann1@gmail.com>
2022-01-24 22:35:56 +0800

    Add `updateMenuInfo`

--- src/main/java/com/controller/MenuServlet.java
+++ src/main/java/com/controller/MenuServlet.java
@@ -2,6 +2,7 @@ package com.controller;
 
 import com.dataaccess.MenuDA;
 import com.model.Menu;
+import com.model.Worker;
 import org.json.JSONObject;
 
 import javax.servlet.*;
@@ -30,7 +31,15 @@ public class MenuServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
 
+        if(action == null) return;
+
+        switch (action.toLowerCase()) {
+            case "updatemenuinfo":
+                updateMenuInfo(request, response);
+                break;
+        }
     }
 
     private static void jsonResponse(HttpServletResponse response, int statusCode, JSONObject json) {
@@ -105,5 +114,62 @@ public class MenuServlet extends HttpServlet {
         json.put("content", menuJson);
 
         jsonResponse(response, 200, json);
+    }
+
+    private static void updateMenuInfo(HttpServletRequest request, HttpServletResponse response) {
+        JSONObject json = new JSONObject();
+        response.setContentType("application/json");
+
+        HttpSession session = request.getSession(false);
+
+        if(session == null || session.getAttribute("workerObj") == null) {
+            json.put("error", "Authorization failed! Please login first.");
+            jsonResponse(response, 401, json);
+            return;
+        }
+
+        Worker worker = (Worker) session.getAttribute("workerObj");
+
+        if(worker.getManagerId() != -1) {
+            json.put("error", "Authorization failed! Only manager can update menu.");
+            return;
+        }
+
+        String idTemp, name, priceTemp, description;
+
+        idTemp = request.getParameter("id");
+        name = request.getParameter("name");
+        priceTemp = request.getParameter("price");
+        description = request.getParameter("description");
+
+        if(idTemp == null || name == null || priceTemp == null || description == null
+            || idTemp.equals("") || name.equals("") || priceTemp.equals("") || description.equals("")) {
+            json.put("error", "Input empty");
+            jsonResponse(response, 400, json);
+            return;
+        }
+
+        int id = -1; double price = -1;
+        try {
+            id = Integer.parseInt(idTemp);
+            price = Double.parseDouble(priceTemp);
+        } catch (Exception err) {
+            err.printStackTrace();
+            json.put("error", "Id must be number / Price must be double");
+            jsonResponse(response, 400, json);
+        }
+
+        if(id == -1 || price == -1) return;
+
+        boolean succeed = MenuDA.updateMenuInfo(id,name,price,description);
+
+        if(succeed) {
+            json.put("message", "Menu has been updated");
+            json.put("menuId", id);
+        } else {
+            json.put("error", "Cannot update menu");
+        }
+
+        jsonResponse(response, succeed ? 200 : 400, json);
     }
 }
